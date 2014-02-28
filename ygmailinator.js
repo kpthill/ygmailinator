9eb9adc8da60a943f956db3b3a28147cacde6759
kpthill <kevin.patrick.thill@gmail.com>
2014-02-27 20:50:46 -0800

    IT DOESNT WORK :-(

--- ygmailinator.js
+++ ygmailinator.js
@@ -1,7 +1,43 @@
-window.onload = function() {
-   document.getElementsByTagName('body')[0].onkeydown = function(e) {
-     e.preventDefault();
-     e.stopPropagation();
-     console.log("YGMAILINATOR: ", e);
-   }
-};
+function keypress(k) {
+  var oEvent = document.createEvent('KeyboardEvent');
+
+  // Chromium Hack
+  Object.defineProperty(oEvent, 'keyCode', {
+    get : function() {
+      return this.keyCodeVal;
+    }
+  });
+  Object.defineProperty(oEvent, 'which', {
+    get : function() {
+      return this.keyCodeVal;
+    }
+  });
+
+  if (oEvent.initKeyboardEvent) {
+    oEvent.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, k, k);
+  } else {
+    oEvent.initKeyEvent("keydown", true, true, document.defaultView, false, false, false, false, k, 0);
+  }
+
+  oEvent.keyCodeVal = k;
+
+  if (oEvent.keyCode !== k) {
+    alert("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
+  }
+
+  document.dispatchEvent(oEvent);
+}
+
+$("document").ready(function() {
+  console.log("doit");
+
+  document.addEventListener("keydown", function(e) {
+    console.log(e);
+
+    if (e.keyCode == 74) {
+      console.log("going");
+      keypress(40);
+    }
+
+  }, true);
+});
